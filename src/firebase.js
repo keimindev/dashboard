020efc998abf13daa109b1b2ccfef641df8475b9
MIN <65863834+kxxrxmxn@users.noreply.github.com>
2021-08-09 17:18:51 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/firestore"
 
 
 const firebaseConfig = {
-        apiKey: "AIzaSyCt3qm1hdVs0Z3kp5UJr0TWYLQkmrnpQtU",
-        authDomain: "board-9ea9c.firebaseapp.com",
-        projectId: "board-9ea9c",
-        storageBucket: "board-9ea9c.appspot.com",
-        messagingSenderId: "502435796985",
-        appId: "1:502435796985:web:a0ddab10b807723c348f28"
+        apiKey: "ur KEY",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "UR id",
+        appId: ""
 }
 
 export default firebase.initializeApp(firebaseConfig);
